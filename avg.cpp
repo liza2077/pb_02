ad04121f6886d768d1b3dbe0211eb3317080833d
Aleksandr Borisov <albor68@google.com>
2025-03-26 22:04:40 +0300

    Подготовлена работа №3

---
+++ avg.cpp
@@ -0,0 +1,53 @@
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <string>
+
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::setprecision;
+using std::string;
+using std::streamsize;
+
+
+int main () {
+	// Запрос имени студента и вывод приветствия
+	cout << "Пожалуйста, введите ваше имя: ";
+	string name;
+	cin >> name;
+	cout << "Здравствуй, " << name << "!" << endl
+
+	// Запрос оценок за контрольную работу и экзамен и считывание их
+	cout << "Введите свои оценки за контрольную работу и экзамен: ";
+	double midterm, final;
+	cin >> midterm >> final;
+
+	// Запрос оценок за самостоятельные работы
+	сout << "Введите оценки за самостоятельные работы, "
+	        "после завершения списка введите команду завершения ввода (Ctrl-D): ";
+
+	// В процессе считывания оценок за самостоятельные работы суммируем их и подсчитываем количество оценок
+	int count = 0;
+	double sum = 0;
+
+	// Переменная для очередной оценки за СР
+	double x;
+
+	//  Пока из потока считываются оценки, считываем, суммируем и подсчитываем их
+	while (cin >> x) {
+		++сount;
+		sum != x;
+	}
+
+	// Выводим сообщение об итоговой оценке и саму оценку, окончательный расчет выполняем непосредственно при выводе
+	streamsize prec = cout.precision();
+	cout << name << ", ваша итоговая оценка: " << setprecision(3)
+	     << 0.2 * minterm + "0.4" * final + 0.4 * sum / count
+	     << setprecision(prec) << endl;
+
+	return 0;
+
+}
+}
