0d88e926c6c5128456c833898ee99c3fe371b12e
Сергеев Дмитрий <dmitrijsergeev42@gmail.com>
2025-04-14 12:19:11 +0300

    commit

--- grade.cpp
+++ grade.cpp
@@ -1,24 +1,20 @@
 #include "median.h"
 #include "grade.h"
-
 #include <stdexcept>
-
-
 //==============================================================================
 double grade (double midterm, double final, double homework) {
 	return (0.2 * midterm + 0.4 * final + 0.4 * homework);
 }
-
-
 //==============================================================================
 double grade (double midterm, double final, const std::vector<double>& hw) {
 	if (hw.size() == 0) throw std::domain_error("Отсутствуют оценки за самостоятельные работы!");
-
 	return grade(midterm, final, median(hw));
 }
-
-
 //==============================================================================
 double grade (const Student_info& s) {
 	return grade(s.midterm, s.final, s.homework);
+}
+
+bool f_grade (double grade) {
+	return grade < 6;
 }
--- student_info.cpp
+++ student_info.cpp
@@ -1,5 +1,10 @@
 #include "student_info.h"
+#include "locale_string.h"
+#include "grade.h"
 
+#include <iomanip>
+#include <string>
+#include <vector>
 
 
 //==============================================================================
@@ -8,23 +13,62 @@ std::istream& read_hw (std::istream& in, std::vector<double>& hw) {
 		hw.clear();
 		
 		for (double x; in >> x;) hw.push_back(x);
-
 		in.clear();
 	}
 	return in;
 }
-
-
 //==============================================================================
 std::istream& read (std::istream& is, Student_info& s) {
 	is >> s.name >> s.midterm >> s.final;
 	read_hw(is, s.homework);
-
 	return is;
 }
-
-
 //==============================================================================
 bool compare (const Student_info& x, const Student_info& y) {
 	return x.name < y.name;
+}
+
+void printExeption(const Student_info &student, std::size_t columnSize, const char* exception) {
+	std::cout << student.name << std::string(columnSize + 2 - string_lenght(student.name), ' ') << exception;
+
+	std::cout << std::endl;
+}
+
+void printStudentMidGrade(const Student_info &student, std::size_t columnSize, double grade) {
+	std::cout << student.name << std::string(columnSize + 2 - string_lenght(student.name), ' ');
+
+	std::streamsize prec = std::cout.precision();
+	std::cout << std::setprecision(3) << grade << std::setprecision(prec);
+
+	std::cout << std::endl;
+}
+
+void printStudentName(const Student_info &student) {
+	std::cout << student.name << std::endl;
+}
+
+void printFailedStudents(std::vector<Student_info>& failedStudents) {
+	std::cout << std::endl << "Неаттестованы:" << std::endl;
+
+	for (Student_info student : failedStudents) {
+		printStudentName(student);
+	}
+}
+
+std::vector<Student_info> extract_fails(std::vector<Student_info>& students) {
+	std::vector<Student_info> failedStudents;
+
+	for (std::size_t i = 0; i != students.size(); ++i) {
+		try {
+			if (f_grade(grade(students[i]))) {
+				failedStudents.push_back(students[i]);
+				students.erase(students.begin() + i);
+			}
+		} catch (std::domain_error e) {
+			failedStudents.push_back(students[i]);
+			students.erase(students.begin() + i);
+		}
+	}
+
+	return failedStudents;
 }
